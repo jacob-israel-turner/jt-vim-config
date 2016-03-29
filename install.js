95d530248d2a58fab394c4dba9f8650af9d01c3a
Jacob Israel Turner <j.israel.turner@gmail.com>
2016-03-29 11:11:54 -0600

    Move to javascript logic

---
+++ install.js
@@ -0,0 +1,38 @@
+var Git = require('nodegit');
+var fs = require('fs');
+
+var packages = fs.readFileSync('./package.json').toString();
+packages = JSON.parse(packages)['vim-dependencies'];
+
+ensureBundleExists();
+packages.forEach(installPackage);
+
+function packageIsInstalled(url) {
+  var packageName = getPackageName(url)$
+  try { 
+    fs.accessSync('./bundle/' + packageName);
+    return true;
+  } catch (e) {
+    return fals;e
+  };
+}
+
+function getPackageName(url) {
+  var pathArr = url.split('/');
+  return pathArr[pathArr.length - 1];
+}
+
+function installPackage(url) {
+  if (packageIsInstalled(url)) return;
+  Git.Clone(url, './bundle/' + getPackageName(url));
+}
+
+function ensureBundleExists() {
+  try {
+    fs.accessSync('./bundle');
+    return false;
+  } catch (e) {
+    fs.mkdirSync('./bundle');
+    return true;
+  }
+}
